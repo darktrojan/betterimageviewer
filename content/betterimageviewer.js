33b66e17e6db72be8240586da963e0a3c6ae5c86
Geoff Lankow <geoff@darktrojan.net>
2015-12-07 16:31:09 +1300

    Create Better Image Viewer

---
+++ content/betterimageviewer.js
@@ -0,0 +1,76 @@
+let BetterImageViewer = {
+	_currentZoom: 0,
+	init: function() {
+		this.image = document.body.firstElementChild;
+		this.image.style.backgroundColor = 'transparent';
+		this.image.style.backgroundImage = 'none';
+
+		this.image.addEventListener('load', this);
+		this.image.addEventListener('click', this);
+		addEventListener('wheel', this);
+	},
+	get zoom() {
+		return this._currentZoom;
+	},
+	set zoom(z) {
+		this._currentZoom = z;
+		this.image.width = Math.pow(2, z / 4) * this.image.naturalWidth;
+		this.image.height = Math.pow(2, z / 4) * this.image.naturalHeight;
+
+		this.image.classList.remove('shrinkToFit');
+		this.image.classList.remove('overflowing');
+		if (z > 0 || z === 0 && (this.image.naturalWidth > document.body.clientWidth || this.image.naturalHeight > document.body.clientHeight)) {
+			this.image.classList.add('overflowing');
+		} else if (z < 0) {
+			this.image.classList.add('shrinkToFit');
+		}
+	},
+	zoomToFit: function() {
+		document.body.style.overflow = 'hidden';
+		let minZoomX = Math.floor((Math.log2(document.body.clientWidth) - Math.log2(this.image.naturalWidth)) * 4);
+		let minZoomY = Math.floor((Math.log2(document.body.clientHeight) - Math.log2(this.image.naturalHeight)) * 4);
+		this.zoom = Math.min(minZoomX, minZoomY, 0);
+		document.body.style.overflow = null;
+	},
+	toggleBackground: function() {
+		if (!document.body.style.backgroundImage) {
+			document.body.style.backgroundColor = '#e5e5e5';
+			document.body.style.backgroundImage = 'url("chrome://global/skin/media/imagedoc-lightnoise.png")';
+		} else {
+			document.body.style.backgroundColor = null;
+			document.body.style.backgroundImage = null;
+		}
+	},
+	handleEvent: function(event) {
+		switch (event.type) {
+		case 'load':
+			this.zoomToFit();
+			break;
+		case 'click':
+			if (this.zoom === 0) {
+				this.zoomToFit();
+				return;
+			}
+			/* falls through */
+		case 'wheel':
+			let bcr = this.image.getBoundingClientRect();
+			let x = (event.clientX - bcr.left) / bcr.width;
+			let y = (event.clientY - bcr.top) / bcr.height;
+
+			if (event.type == 'click') {
+				this.zoom = 0;
+			} else if (event.deltaY < 0) {
+				this.zoom++;
+			} else {
+				this.zoom--;
+			}
+
+			bcr = this.image.getBoundingClientRect();
+			document.body.scrollTo(bcr.width * x - event.clientX, bcr.height * y - event.clientY);
+
+			event.preventDefault();
+			break;
+		}
+	}
+};
+BetterImageViewer.init();
---
+++ content/process.js
@@ -0,0 +1,46 @@
+/* globals Components, Services, addMessageListener, removeMessageListener */
+Components.utils.import('resource://gre/modules/Services.jsm');
+
+let listener = {
+	_messages: [
+		'BetterImageViewer:disable'
+	],
+	_notifications: [
+		'content-document-global-created'
+	],
+	init: function() {
+		for (let m of this._messages) {
+			addMessageListener(m, this);
+		}
+		for (let n of this._notifications) {
+			Services.obs.addObserver(this, n, false);
+		}
+	},
+	destroy: function() {
+		for (let m of this._messages) {
+			removeMessageListener(m, this);
+		}
+		for (let n of this._notifications) {
+			Services.obs.removeObserver(this, n, false);
+		}
+	},
+	receiveMessage: function(message) {
+		switch (message.name) {
+		case 'BetterImageViewer:disable':
+			this.destroy();
+			break;
+		}
+	},
+	observe: function(subject) {
+		let doc = subject.document;
+		if (doc.toString() != '[object ImageDocument]') {
+			return;
+		}
+
+		let s = doc.createElement('script');
+		s.setAttribute('src', 'chrome://betterimageviewer/content/betterimageviewer.js');
+		s.setAttribute('type', 'application/javascript;version=1.8');
+		doc.body.appendChild(s);
+	}
+};
+listener.init();
